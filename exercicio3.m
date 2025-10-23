f5e6dbbc758c924f5b22203124a008780138ce0f
grinberganita <grinberganita@gmail.com>
2025-10-23 16:28:25 -0300

    Update exercicio3.m

--- exercicio3.m
+++ exercicio3.m
@@ -1,16 +1,21 @@
-function [mu] = exercicio3(t, NC)
+tempo_quad = t.^2
 
-% t: vetor de tempo com instâncias das medições
-% NC: medida do número de células a cada instante t
+N = length(t)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+soma_tempo = sum(t)
+soma_tempo_quad = sum(tempo_quad) 
 
-% digite seu codigo aqui
+ln_celulas = log(NC)
 
+soma_ln_celulas = sum(ln_celulas) 
+
+tempo_X_ln_celulas = t.*ln_celulas
+
+soma_do_produto = sum(tempo_X_ln_celulas)
+
+a1 = (N*soma_do_produto - soma_tempo*soma_ln_celulas)/(N*soma_tempo_quad - soma_tempo^2)
+
+a0 = soma_ln_celulas/N - a1*(soma_tempo/N)
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = 0;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-endfunction
+mu = a1;
